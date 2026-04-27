9b1c62bbdf512acdcefca9f3f0d752fa8696d57b
oumeima-ibnelfekih <oumeima.ibnelfekih@esprit.tn>
2026-04-27 20:22:31 +0100

    config rabbitmq

--- src/main/java/esprit/jobms/JobService.java
+++ src/main/java/esprit/jobms/JobService.java
@@ -1,15 +1,39 @@
 package esprit.jobms;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobService {
-    @Autowired
 
     private JobRepository jobRepository;
+
+    private final JobProducer jobProducer;
+    private static final Logger log = LoggerFactory.getLogger(JobService.class);
+    public JobService(JobRepository jobRepository, JobProducer jobProducer) {
+        this.jobRepository = jobRepository;
+        this.jobProducer = jobProducer;
+    }
+    /** Sauvegarde d'un Job dans la base de données et envoi d’un DTO à RabbitMQ **/
+
+    //si save réussit mais l’envoi échoue, on aura un enregistrement sans message
+    @Transactional
+    public Job saveAndSendJob(Job jobEntity) {
+        Job savedJob = jobRepository.save(jobEntity);
+        log.info("Job sauvegardé en base : {}", savedJob.getService());
+        // Construire un DTO à envoyer
+        JobDTO jobDTO = new JobDTO(savedJob.getId(), savedJob.getService(), savedJob.isEtat());
+        // Envoi asynchrone via RabbitMQ
+        jobProducer.sendJob(jobDTO);
+        return savedJob;
+
+    }
+
+
     public Job addJob(Job job) {
         return jobRepository.save(job);
     }
